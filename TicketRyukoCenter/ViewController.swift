9f47a0538f3e3f56d702465f38f71c5c606842f7
SeinaKonishi <fzf55747@kwansei.ac.jp>
2022-03-06 20:41:10 +0900

    Initial Commit

---
+++ TicketRyukoCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicketRyukoCenter
+//
+//  Created by SeinaKonishi on 2022/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
