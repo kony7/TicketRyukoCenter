8048afc83468233c3f178bb03a1bcbc9599d582c
SeinaKonishi <fzf55747@kwansei.ac.jp>
2022-03-07 16:30:24 +0900

    UIパーツの配置DONE

--- TicketRyukoCenter/ViewController.swift
+++ TicketRyukoCenter/ViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var table: UITableView!
+    
+    var titleArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        table.dataSource = self
+        table.delegate = self
+    
+        titleArray = ["肩たたき券","お誕生日チケット"]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = titleArray[indexPath.row]
+        return cell!
     }
 
 
