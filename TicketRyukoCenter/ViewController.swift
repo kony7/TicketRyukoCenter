fcc9e37cae7ada0ab99d2b1107a27a9243045480
SeinaKonishi <fzf55747@kwansei.ac.jp>
2022-03-08 12:58:14 +0900

    配列になにも入ってなかった時の実装完了

--- TicketRyukoCenter/ViewController.swift
+++ TicketRyukoCenter/ViewController.swift
@@ -20,16 +20,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         table.dataSource = self
         table.delegate = self
     
-        titleArray = ["肩たたき券","お誕生日チケット"]
+       // titleArray = []
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if titleArray.count == 0{
+            return 1
+        }else{
         return titleArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        if titleArray.count == 0{
+            
+            //ここを画像にする
+            cell?.textLabel?.text = "チケットが一枚もありません"
+            
+        }else{
+            
+            //ここを画像にする
         cell?.textLabel?.text = titleArray[indexPath.row]
+            
+        }
         return cell!
     }
 
