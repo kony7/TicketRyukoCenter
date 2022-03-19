179d25c24bf64b2cab424719ee4ff2754bbc32f2
SeinaKonishi <fzf55747@kwansei.ac.jp>
2022-03-19 15:54:37 +0900

    Realmに追加されるようになった。これを送った側と受け取った側で判別して受け取った側だけがTableViewに表示されるようにこれから変更していく

--- TicketRyukoCenter/MakeTicketViewController.swift
+++ TicketRyukoCenter/MakeTicketViewController.swift
@@ -11,22 +11,26 @@ import RealmSwift
 
 class MakeTicketViewController: UIViewController, UITextFieldDelegate {
     
+    //firebaseとrealmのインスタンス化
     let firestore = Firebase.Firestore.firestore()
     let realm = try! Realm()
+    
+    //発券番号になるUUID(IDを自動で発行してくれる関数かな？)を宣言
     let uuid = UUID()
 
-    
+    //カードのデザインを選ぶボタンの宣言
     @IBOutlet var designCard1: UIButton!
     @IBOutlet var designCard2: UIButton!
     @IBOutlet var designCard3: UIButton!
     @IBOutlet var designCard4: UIButton!
     
+    //タイトル・送信者・コメント・期限を入力するテキストフィールドとDataPickerの宣言
     @IBOutlet var titleTextField:UITextField!
     @IBOutlet var senderTextField:UITextField!
     @IBOutlet var comentTextField:UITextField!
-    
     @IBOutlet var limitDatePicker: UIDatePicker!
     
+    //入力されたタイトル・送信者・コメント・期限・カードデザイン・発券番号を一時的に保存する変数の宣言
     var titleText: String = ""
     var senderText: String = ""
     var comentText: String = ""
@@ -34,10 +38,13 @@ class MakeTicketViewController: UIViewController, UITextFieldDelegate {
     var cardSelecte:Int = 0
     var tickedtID: String = ""
     
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         
-        
+        //テキストフィールドを下線だけのデザインに変更
         titleTextField.setUnderLine()
         senderTextField.setUnderLine()
         comentTextField.setUnderLine()
@@ -46,12 +53,10 @@ class MakeTicketViewController: UIViewController, UITextFieldDelegate {
         senderTextField.delegate = self
         comentTextField.delegate = self
         
-        
-        
-        // Do any additional setup after loading the view.
     }
     
 
+    //カードを押したらそのデザインの変数が保存されるようにする
     @IBAction func pinkButton(){
         cardSelecte = 1
     }
@@ -68,19 +73,22 @@ class MakeTicketViewController: UIViewController, UITextFieldDelegate {
         cardSelecte = 4
     }
     
+    //画面遷移の前に準備するもの
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+        //発券番号の発行
         tickedtID = uuid.uuidString
         
-        print("こちらが早く呼び出されているとprepare")
-        
+        //テキストフィールドに入力されている内容などを変数に代入する関数
         textFieldToValue()
 
          // segueのIDを確認して特定のsegueのときのみ動作させる
          if segue.identifier == "toSendViewController" {
-             // 2. 遷移先のViewControllerを取得
+             
+             //遷移先のViewControllerを取得
              let next = segue.destination as? SendViewController
-             // 3. １で用意した遷移先の変数に値を渡す
+             
+             //遷移先の変数に値を渡す
              next?.titleText = self.titleText
              next?.senderText = self.senderText
              next?.comentText = self.comentText
@@ -91,24 +99,29 @@ class MakeTicketViewController: UIViewController, UITextFieldDelegate {
          }
      }
 
+    //prepareから呼ばれるやつ
      @IBAction func tapTransitionButton(_ sender: Any) {
-         // 4. 画面遷移実行
+         
+         //画面遷移実行
          performSegue(withIdentifier: "toSendViewController", sender: nil)
+         
      }
     
+    //Firebaseへの保存とRealmへの保存をボタンに紐づけるためのメソッド
     @IBAction func tapToSaveAndSendData(){
-        
-        print("こちらが早く呼び出されているとIBAction")
         
         sendDataToFirebase()
         saveCardDatatoRealm()
         
     }
     
+    //Firebaseにデータを送るためのメソッド
     func sendDataToFirebase(){
         
+        //テキストフィールドに入力されている内容などを変数に代入する関数
         textFieldToValue()
         
+        //Firebaseに送るために配列型に変更
         let cardData = [
             
             "cardTitle": titleText,
@@ -119,88 +132,50 @@ class MakeTicketViewController: UIViewController, UITextFieldDelegate {
             
         ] as [String : Any]
 
-        //firestore.collection("cards").addDocument(data: cardData)
+        //documentIDをこちらで発行したUUIDに設定
         firestore.collection("cards").document(tickedtID).setData(cardData){ err in
             if let err = err {
                 print("送信できませんでした: \(err)")
             }
         }
-        
-        
-       // firestore.collection("cards").document("cardInfo").setData(["cardTitle": titleTextField.text!])
     }
     
+    
+    //Realmにデータを保存する関数
     func saveCardDatatoRealm(){
         
+        //テキストフィールドに入力されている内容などを変数に代入する関数
         textFieldToValue()
-        
-        let cardSave: RecaiveCard? = read()
-        
-        if cardSave != nil{
-            
-                  try! realm.write{
-                      cardSave!.title = titleText
-                      cardSave!.sender = senderText
-                      cardSave!.limit = limitDateValue
-                      cardSave!.coment = comentText
-                      cardSave!.design = cardSelecte
-                  
-                  }
-            
-            print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true))
-            
-        }else{
-                    
-            let newCard = RecaiveCard()
-            
-            newCard.title = titleText
-            newCard.sender = senderText
-            newCard.limit = limitDateValue
-            newCard.coment = comentText
-            newCard.design = cardSelecte
+               
+        //Realmのクラスの型で新しい定数を宣言し、それぞれに入力した値を代入
+        let newCard = RecaiveCard()
+        newCard.title = titleText
+        newCard.sender = senderText
+        newCard.limit = limitDateValue
+        newCard.coment = comentText
+        newCard.design = cardSelecte
                       
-            try! realm.write{
-                          
-                realm.add(newCard)
-                print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true))
+        //代入した値をRealmに追加
+        try! realm.write{
+              
+            realm.add(newCard)
                   
             }
         }
     
-//        try! realm.write{
-//
-//            cardSave!.title = titleText
-//            cardSave!.sender = senderText
-//            cardSave!.limit = limitDateValue
-//            cardSave!.coment = comentText
-//            cardSave!.design = cardSelecte
-//
-//        }
-    }
-    
-    func read() -> RecaiveCard?{
-        return realm.objects(RecaiveCard.self).first
-    }
-    
+    //テキストフィールドに入力されている内容などを変数に代入する関数
     func textFieldToValue(){
         
-        if titleTextField.text == nil{
+        //もしテキストフィールドの内容が空だったらアラートを鳴らしそれ以外の場合にFirebaseにデータを送る
+        if titleTextField.text == nil && titleTextField.text == ""{
             
             getAlert(alertTitleText: "タイトルが入力されていません")
             return
             
-        }else if senderTextField.text == nil{
+        }else if senderTextField.text == nil && titleTextField.text == ""{
             
             getAlert(alertTitleText: "送信者が入力されていません")
             return
-            
-        }else if comentTextField.text == nil{
-            
-            comentText = ""
-            titleText = self.titleTextField.text!
-            senderText = self.senderTextField.text!
-            comentText = self.comentTextField.text!
-            limitDateValue = self.limitDatePicker.date
             
         }else{
         
@@ -212,24 +187,29 @@ class MakeTicketViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    //アラートを鳴らす関数
     func getAlert(alertTitleText:String){
         
-        
+        //アラートのタイトルには引数を出力し、メッセージにはアラートが出ている原因を出力
         let alert = UIAlertController(title: alertTitleText, message: "必須の項目が入力されていません", preferredStyle: .alert)
         
-        let cancel = UIAlertAction(title: "キャンセル", style: .cancel) { (acrion) in
+        //ボタンは戻るボタンを設定
+        let cancel = UIAlertAction(title: "戻る", style: .cancel) { (acrion) in
             self.dismiss(animated: true, completion: nil)
         }
         
+        //アラートを上記で設定した内容で出す
         alert.addAction(cancel)
-        //ここまで追加
         present(alert, animated: true, completion: nil)
         
     }
     
+    //テキストフィールドに入力中、キーボードでReturnボタンが押されるとキーボードが終われるメソッド
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
         textField.resignFirstResponder()
         return true
+        
     }
     
 
@@ -245,6 +225,7 @@ class MakeTicketViewController: UIViewController, UITextFieldDelegate {
 
 }
 
+//テキストフィールドを下線だけにする関数
 extension UITextField {
     func setUnderLine() {
         // 枠線を非表示にする
--- TicketRyukoCenter/ViewController.swift
+++ TicketRyukoCenter/ViewController.swift
@@ -51,48 +51,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     }
     
-//    func read() -> RecaiveCard?{
-//        return realm.objects(RecaiveCard.self).first
-//    }
-    
+    //テーブルビューのセルの数を指定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        //もしRealm内にデータがなければ1つだけ、あればそのデータの数だけセルを用意
         if cardsList.count == 0{
             
             return 1
             
         }else{
+            
         print(String(cardsList.count) + "がtableView読み込む時のカードリストの数")
         return cardsList.count
            
-            
         }
     }
     
+    //テーブルビューの内容を指定
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        //テーブルビューのセルを宣言
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         
+        //もしRealm内にデータがなければチケットがないことを表示
         if cardsList.count == 0{
             
-            //ここを画像にする
+            //画像も追加
             cell?.textLabel?.text = "チケットが一枚もありません"
             
         }else{
                 
+        //もしRealm内にデータがあればタイトルを表示する
         let cardContent: RecaiveCard = self.cardsList[(indexPath as NSIndexPath).row];
-            //ここを画像にする
+            
+            //画像、送信者、コメント、期限も追加
             cell?.textLabel?.text = cardContent.title
             
             
         }
+        
         return cell!
+        
     }
-    
-//    func read() -> RecaiveCard?{
-//        return realm.objects(RecaiveCard.self).first
-//    }
-
 
 }
 
